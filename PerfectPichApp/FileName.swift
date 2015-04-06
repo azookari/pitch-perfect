0629147010412ac0fae9476cbf142c42bdf48312
azookari <omar_azookari@yahoo.com>
2015-04-05 23:33:27 -0400

    Committed by Udacity

---
+++ PerfectPichApp/FileName.swift
@@ -0,0 +1,13 @@
+//
+//  FileName.swift
+//  PerfectPichApp
+//
+//  Created by Omar Azookari on 4/5/15.
+//  Copyright (c) 2015 Omar Azookari. All rights reserved.
+//
+
+import Foundation
+class FileName: NSObject {
+    var filePathUrl: NSURL!
+    var title: String!
+}
